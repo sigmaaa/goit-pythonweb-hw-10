94e21915991531faf50729829e1fd58c1aaa66c9
Denys Almazov <almazovdenys@gmail.com>
2025-09-15 17:44:24 +0300

    updating schemas with user and token

--- src/schemas.py
+++ src/schemas.py
@@ -31,3 +31,22 @@ class ContactResponse(ContactBase):
     updated_at: Optional[datetime] = None
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+    avatar: str
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
