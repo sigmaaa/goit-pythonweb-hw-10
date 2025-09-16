f79f28db5e04a6c2cd06919de52de59c8b440a67
Denys Almazov <almazovdenys@gmail.com>
2025-09-16 13:56:13 +0300

    added update_avatar functionality

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
+from slowapi import Limiter
+from slowapi.errors import RateLimitExceeded
+from starlette.responses import JSONResponse
 from src.api import utils, contacts, auth, users
+from fastapi.middleware.cors import CORSMiddleware
+
+# Initialize Limiter
+limiter = Limiter(key_func=lambda request: request.client.host)
 
 app = FastAPI()
 
+origins = ["<http://localhost:3000>"]
+
+# Attach middleware
+app.state.limiter = limiter
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include routers
 app.include_router(utils.router, prefix="/api")
 app.include_router(contacts.router, prefix="/api")
 app.include_router(auth.router, prefix="/api")
 app.include_router(users.router, prefix="/api")
+
+
+# Exception handler for rate limiting
+@app.exception_handler(RateLimitExceeded)
+async def rate_limit_handler(request: Request, exc: RateLimitExceeded):
+    return JSONResponse(
+        status_code=429,
+        content={"error": "Перевищено ліміт запитів. Спробуйте пізніше."},
+    )
+
 
 if __name__ == "__main__":
     import uvicorn
--- src/api/users.py
+++ src/api/users.py
@@ -1,10 +1,36 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Request, Depends, File, UploadFile
+from sqlalchemy.ext.asyncio import AsyncSession
 from src.schemas import User
 from src.services.auth import get_current_user
+from slowapi import Limiter
+from src.services.upload_file import UploadFileService
+from src.conf.config import config
+from src.database.db import get_db
+from src.services.users import UserService
 
 router = APIRouter(prefix="/users", tags=["users"])
+limiter = Limiter(key_func=lambda request: request.client.host)
 
 
-@router.get("/me", response_model=User)
-async def me(user: User = Depends(get_current_user)):
+@router.get(
+    "/me", response_model=User, description="No more than 10 requests per minute"
+)
+@limiter.limit("10/minute")
+async def me(request: Request, user: User = Depends(get_current_user)):
+    return user
+
+
+@router.patch("/avatar", response_model=User)
+async def update_avatar_user(
+    file: UploadFile = File(),
+    user: User = Depends(get_current_user),
+    db: AsyncSession = Depends(get_db),
+):
+    avatar_url = UploadFileService(
+        config.CLD_NAME, config.CLD_API_KEY, config.CLD_API_SECRET
+    ).upload_file(file, user.username)
+
+    user_service = UserService(db)
+    user = await user_service.update_avatar_url(user.email, avatar_url)
+
     return user
